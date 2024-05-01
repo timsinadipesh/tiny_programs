2e75fb1a75c23666308702d3dac96b49c20241ef
creasac2 <timsinadipesh77@gmail.com>
2024-05-01 14:11:37 +0545

    commit

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -7,7 +7,7 @@ def print_pascal_triangle(height):
         val = 1
         for j in range(i + 1):
             print(val, end=" ")
-            val = val * (i - j) // (j + 1)
+            val = val * (i - j) // (j + 1) # binomial coefficient
         print()
 
 print_pascal_triangle(10)
