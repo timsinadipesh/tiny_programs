3c5163524d7b54007eee5d27fa2a93d56cafde9c
creasac2 <timsinadipesh77@gmail.com>
2024-05-01 11:32:23 +0545

    commit

---
+++ pascal_triangle.py
@@ -0,0 +1,13 @@
+# prints pascal's triangle for the given height
+
+def print_pascal_triangle(height):
+    for i in range(height):
+        space = " " * (height - (i + 1))
+        print(space, end="")
+        val = 1
+        for j in range(i + 1):
+            print(val, end=" ")
+            val = val * (i - j) // (j + 1)
+        print()
+
+print_pascal_triangle(10)
